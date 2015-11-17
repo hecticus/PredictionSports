cb4ea006016d6795729174b5bcaa95194e144628
Jose Ortegano <jose.ortegano@hecticus.com>
2015-11-17 16:54:03 -0430

    Build Script

--- Apps/Tim/app/futbol-brasil/update_cordova_platform.sh
+++ Apps/Tim/app/futbol-brasil/update_cordova_platform.sh
@@ -4,19 +4,20 @@ cordova platform rm android
 echo "###Remove iOS"
 cordova platform rm ios
 echo "###Remove FB plugin"
-#cordova plugin rm com.phonegap.plugins.facebookconnect
+cordova plugin rm com.phonegap.plugins.facebookconnect
+cordova plugin rm phonegap-facebook-plugin
 echo "###Add Android"
 cordova platform add android
 echo "###Add iOS"
 cordova platform add ios
 echo "###Add FB plugin"
-#cordova -d plugin add https://github.com/Wizcorp/phonegap-facebook-plugin --variable APP_ID="320314531485580" --variable APP_NAME="FutbolBrasil"
-echo "###Adjust Folder Permission: "
-chown -R joe platforms/
-chown -R joe plugins/
-chown -R joe www/
-chmod -R 775 platforms/
-chmod -R 775 plugins/
-chmod -R 775 www/
+cordova -d plugin add https://github.com/Wizcorp/phonegap-facebook-plugin --variable APP_ID="1379325579064871" --variable APP_NAME="TIM Palpites"
+#echo "###Adjust Folder Permission: "
+#chown -R joe platforms/
+#chown -R joe plugins/
+#chown -R joe www/
+#chmod -R 775 platforms/
+#chmod -R 775 plugins/
+#chmod -R 775 www/
 
 
