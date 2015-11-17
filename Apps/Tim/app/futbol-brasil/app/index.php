dbf95facea44e358a27e1e0feb688ae71173c4d4
Alí Buenaño <alidanielbs@gmail.com>
2015-11-17 16:20:30 -0500

    Update index.php
    TIM - SHARE: PRINT HTTP_REFERER

--- Apps/Tim/app/futbol-brasil/app/index.php
+++ Apps/Tim/app/futbol-brasil/app/index.php
@@ -1,12 +1,8 @@
 <?php
 	  $get =  file_get_contents('http://tim.sports.hecticus.com/newsapi/v1/news/get/'.$_REQUEST['id'].'?timezoneName=GMT-0500&upstreamChannel=Web&api_password=1234)9|1');
 		$data = (Object)json_decode($get);
-		
-	    if(stripos($_server['HTTP_REFERER'], 'facebook') !== false) {
-      echo 'facebook';
-    } else {
-      echo 'no facebook';
-    }
+	
+    echo strpos('facebook',$_SERVER[HTTP_REFERER])
 ?>
 
 <!DOCTYPE html>
