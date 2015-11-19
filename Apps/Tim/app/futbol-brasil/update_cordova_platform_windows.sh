e694fa5429fc0acc25a0106f37ea930eb7e3e1af
ali.buenano <alidanielbs@gmail.com>
2015-11-19 18:27:11 -0500

    TIM APP: Double  Alert at Friends' Sectiong

--- Apps/Tim/app/futbol-brasil/update_cordova_platform_windows.sh
+++ Apps/Tim/app/futbol-brasil/update_cordova_platform_windows.sh
@@ -1,6 +1,6 @@
 cordova platform rm android
 cordova plugin rm com.phonegap.plugins.facebookconnect
 cordova platform add android
-cordova -d plugin add https://github.com/Wizcorp/phonegap-facebook-plugin --variable APP_ID="1379325579064871" --variable APP_NAME="timfutebol"
+cordova -d plugin add https://github.com/Wizcorp/phonegap-facebook-plugin --variable APP_ID="1379325579064871" --variable APP_NAME="TIM Palpites"
 
 
