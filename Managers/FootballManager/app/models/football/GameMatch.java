e3cf61332179c57f01acae5c0e972012e4bc835c
palenge@palenge.com <palenge@palenge.com>
2018-06-13 20:07:55 -0500

    hous fix

--- Managers/FootballManager/app/models/football/GameMatch.java
+++ Managers/FootballManager/app/models/football/GameMatch.java
@@ -369,7 +369,7 @@ public class GameMatch extends FootballModel {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
         df.setTimeZone(timeZone);
         Calendar gameMatchDate = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddhhmmss", TimeZone.getTimeZone("UTC")));
+        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddHHmmss", TimeZone.getTimeZone("UTC")));
         json.put("date",df.format(gameMatchDate.getTime()));
         if(phase != null) {
             json.put("phase",phase.getIdPhases());
@@ -395,7 +395,7 @@ public class GameMatch extends FootballModel {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
         df.setTimeZone(timeZone);
         Calendar gameMatchDate = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddhhmmss", TimeZone.getTimeZone("UTC")));
+        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddHHmmss", TimeZone.getTimeZone("UTC")));
         json.put("date",df.format(gameMatchDate.getTime()));
         if(phase != null) {
             json.put("phase",phase.getIdPhases());
@@ -429,7 +429,7 @@ public class GameMatch extends FootballModel {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
         df.setTimeZone(timeZone);
         Calendar gameMatchDate = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddhhmmss", TimeZone.getTimeZone("UTC")));
+        gameMatchDate.setTime(DateAndTime.getDate(date, date.length() == 8 ? "yyyyMMdd" : "yyyyMMddHHmmss", TimeZone.getTimeZone("UTC")));
         json.put("date",df.format(gameMatchDate.getTime()));
         json.put("home_team",homeTeam.toJsonSimple());
         json.put("away_team",awayTeam.toJsonSimple());
@@ -476,7 +476,7 @@ public class GameMatch extends FootballModel {
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
         df.setTimeZone(timeZone);
         Calendar gameMatchDate = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        gameMatchDate.setTime(DateAndTime.getDate(date, date.length()==8?"yyyyMMdd":"yyyyMMddhhmmss", TimeZone.getTimeZone("UTC")));
+        gameMatchDate.setTime(DateAndTime.getDate(date, date.length()==8?"yyyyMMdd":"yyyyMMddHHmmss", TimeZone.getTimeZone("UTC")));
         json.put("date",df.format(gameMatchDate.getTime()));
         if(homeTeam == null){
             ObjectNode obj = Json.newObject();
