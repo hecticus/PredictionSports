a397bfe87832b9fe0696728d71720cce8c98cf65
PALENGE <palenge@gmail.com>
2020-01-16 22:49:59 +0100

    no message

--- Managers/FootballManager/app/backend/jobs/opta/OptaProcess.java
+++ Managers/FootballManager/app/backend/jobs/opta/OptaProcess.java
@@ -37,6 +37,8 @@ public class OptaProcess extends ProcessAbstract {
         super.process(args);
         language = Language.getByID(300);
 
+        Utils.printToLog(Utils.class, "", "Iniicio Proceso OPTA: ", true, e, "support-level-1", Config.LOGGER_ERROR);
+
         TournamentCalendarRequest tournamentCalendarRequest = null;
         tournamentCalendarRequest = optaRepository.GetTournamentCalendar();
 
@@ -54,6 +56,7 @@ public class OptaProcess extends ProcessAbstract {
             }
         }
 
+        Utils.printToLog(Utils.class, "", "Proceso finalizo Correctamente Proceso OPTA: ", true, e, "support-level-1", Config.LOGGER_ERROR);
         System.out.println("Proceso finalizo Correctamente");
     }
 
