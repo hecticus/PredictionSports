91c1535fd9ee9c4bbbf4ee886055c3efa48e889a
PALENGE <palenge@gmail.com>
2022-07-13 20:09:53 +0200

    bliveupload

---
+++ Apps/MasSports/gpaapi/server/app/modeles/BliveActivity.java
@@ -0,0 +1,52 @@
+package modeles;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+@Entity
+public class BliveActivity extends Model {
+    @Id
+    private Long id;
+    @Column()
+    private String clickId;
+    @Column()
+    private String date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClickId() {
+        return clickId;
+    }
+
+    public void setClickId(String clickId) {
+        this.clickId = clickId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public BliveActivity(String clickId) {
+        this.clickId = clickId;
+        String DATE_FORMAT = "yyyyMMddHHmmss";
+        SimpleDateFormat sdf =
+                new SimpleDateFormat(DATE_FORMAT);
+        Calendar c1 = Calendar.getInstance(); // today
+        this.date =  sdf.format(c1.getTime());
+    }
+}
