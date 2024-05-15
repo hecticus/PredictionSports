995be00506777acb2cf2a8b3ed3742f24d816c2d
palenge <palenge@users.noreply.github.com>
2024-05-15 14:16:36 +0200

    added 36

--- Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoApplandController.java
+++ Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoApplandController.java
@@ -31,6 +31,7 @@ import views.html.ciudadjuego.tyc;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -128,6 +129,7 @@ public class CiudadJuegoApplandController extends Controller {
     }
 
     public Result RedirectFromDigitel(String id, String red, String msisdn) {
+        msisdn = new BigInteger(msisdn, 36).toString();  //Long.valueOf(msisdn, 36).toString();// Integer.toString(msisdn, 36);
         Result rutaRedirect = getResult(msisdn);
         if (rutaRedirect != null) return rutaRedirect;
         return ok();
