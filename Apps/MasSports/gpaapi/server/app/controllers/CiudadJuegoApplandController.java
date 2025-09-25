78ddabebeda928490a9261d86c7ac51b50cc6840
Fernando Alvarez <palenge@gma>
2025-09-25 15:06:24 +0200

    Remove unnecessary query parameter from subscription redirect URLs in CiudadJuegoApplandController and CiudadJuegoNativeController

--- Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoApplandController.java
+++ Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoApplandController.java
@@ -112,7 +112,7 @@ public class CiudadJuegoApplandController extends Controller {
     }
 
     public Result LoginRedirect() throws MalformedURLException {
-        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg&s=null");
+        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg");
     }
 
     public Result Login() throws MalformedURLException {
@@ -158,7 +158,7 @@ public class CiudadJuegoApplandController extends Controller {
         }
 
 
-        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg&s=null");
+        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg");
     }
 
     public Result RedirectFromDigitel(String id, String red, String msisdn) {
--- Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoNativeController.java
+++ Apps/MasSports/gpaapi/server/app/controllers/CiudadJuegoNativeController.java
@@ -36,7 +36,7 @@ public class CiudadJuegoNativeController extends Controller {
 
 
     public Result LoginRedirect() throws MalformedURLException {
-        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg&s=null");
+        return redirect("https://gprs.digitel.com.ve/contenido/subscription?idSc=9424&ac=reg");
     }
 
     public Result Login() throws Exception {
