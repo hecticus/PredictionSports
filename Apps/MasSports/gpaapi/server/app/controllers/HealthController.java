fd5e7781cd4cf5cd01f272cd5c45255ff8152f34
Fernando Alvarez <palenge@gma>
2026-06-23 18:11:39 +0200

    Add HealthController and define health check route

---
+++ Apps/MasSports/gpaapi/server/app/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class HealthController extends Controller {
+
+    private static final String VERSION = "1.0.0";
+
+    public Result health() {
+        return ok("{\"status\":\"ok\",\"version\":\"" + VERSION + "\"}").as("application/json");
+    }
+}
