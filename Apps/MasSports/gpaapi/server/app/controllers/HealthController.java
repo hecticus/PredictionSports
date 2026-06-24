2ff99cf8fb8641852e0bb038e3d06fea71fb4719
Fernando Alvarez <palenge@gma>
2026-06-24 17:25:22 +0200

    Update HealthController version to 1.0.1

--- Apps/MasSports/gpaapi/server/app/controllers/HealthController.java
+++ Apps/MasSports/gpaapi/server/app/controllers/HealthController.java
@@ -5,7 +5,7 @@ import play.mvc.Result;
 
 public class HealthController extends Controller {
 
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.0.1";
 
     public Result health() {
         return ok("{\"status\":\"ok\",\"version\":\"" + VERSION + "\"}").as("application/json");
